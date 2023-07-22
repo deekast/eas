4662587cc99ce2cc0407956109713e1d5ea75187
deekast <96024516+deekast@users.noreply.github.com>
2023-07-22 11:07:04 -0400

    added a button and prompt to choose grid size

--- java.js
+++ java.js
@@ -2,20 +2,26 @@
 // make a loop that runs to a number in this case 16 (later can be replaced with a user input)
 //this loop will make boxes until it runs into the number 
 //outer loop makes X rows and the inner loop makes x boxes inside the rows 
+//when boxes are created event listeners are added to turn the boxes black when moused over 
 
 const container = document.querySelector('#container');
 
-function createBoxes(numBox) {
+function createBoxes() {
+  let numBox = prompt('how many boxes do you want', 100);
   for (let i = 0; i < numBox; i++) {
     const row = container.appendChild(document.createElement('div'));
     for (let j = 0; j < numBox; j++) {
       const square = document.createElement('div');
       square.className = 'box';
+      square.addEventListener("mouseover", function(){
+        this.style.backgroundColor = "black";
+      });
       row.appendChild(square);
     }
   }
 }
 
-createBoxes(10);
+
+// createBoxes(50);
 
 
