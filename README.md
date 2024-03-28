1c69c3473848523a53bc01ab96307dddf8088e4c
deekast <96024516+deekast@users.noreply.github.com>
2024-03-28 10:50:16 -0400

    added requirements for flatiron project

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Etch-A-Sketch
+
+
+To do 
+
+- put in a refresh box 
+- center the box 
+- make iridescent/rainbow
+- allow users to make their own DB entry 
+
+advanced to do 
+- saves! 
+
---
+++ index.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const gridContainer = document.getElementById('gridContainer');
+    const userList = document.getElementById('userList');
+
+    function createBoxes() {
+        let numBox = prompt('how many boxes do you want', 100);
+        for (let i = 0; i < numBox; i++) {
+          const row = container.appendChild(document.createElement('div'));
+          for (let j = 0; j < numBox; j++) {
+            const square = document.createElement('div');
+            square.className = 'box';
+            square.addEventListener("mouseover", function(){
+              this.style.backgroundColor = "black";
+            });
+            row.appendChild(square);
+          }
+        }
+      }
+      
+
+
+    function fetchUsers() {
+        fetch('http://localhost:3000/users')
+        .then(response => response.json())
+       .then(displayUsers)
+    }
+    
+  
+// Function to display users called by the fetchUsers function 
+function displayUsers(users) {
+    const userList = document.getElementById('userList');
+    userList.innerHTML = ''; // clear previous user list so list doesnt keep repeating  if you click button more than one time
+    users.forEach(user => {
+        const userItem = document.createElement('li');
+        const userDetails = `${user.username} : ${user.firstName} ${user.lastName} - ${user.email}`;
+        userItem.textContent = userDetails;
+        userList.appendChild(userItem);
+    });
+
+    userList.style.display = 'block'; // change the html display style from none to block 
+    gridContainer.style.display = 'none'; // should get rid of grid but doesnt need to troubleshoot 
+}
+
+  //button event listeners 
+
+    document.getElementById('refreshButton').addEventListener('click', createBoxes)
+  
+
+    document.getElementById('usersButton').addEventListener('click', fetchUsers);
+  });
+  
